de3f3dec8a193ceca55402eec9901a19c72ee705
Julio Suarez <julio.suarez91@gmail.com>
2023-11-26 04:37:32 -0400

    dominio

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@gmail.com') && $this->hasVerifiedEmail();
+        return str_ends_with($this->email, '@julicosuarez.tech');
     }
     /**
      * The attributes that are mass assignable.
