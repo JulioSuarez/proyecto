e963055d0ddd977972ada2fba1ecccd458ca5482
Cristian Cuellar <ccuellar260@gmail.com>
2023-12-03 09:17:23 -0400

    ultimos cambios

--- resources/js/streamjs/ia_mover_cara.js
+++ resources/js/streamjs/ia_mover_cara.js
@@ -1,62 +1,79 @@
-let ia_video = document.getElementById('ia_video');
+// let ia_video = document.getElementById('ia_video');
 let ip_mensaje = document.getElementById('ip_mensaje');
 let butom_ia = document.getElementById('butom_ia');
-let  bt_mostrar_video = document.getElementById(' bt_mostrar_video');
+let bt_mostrar_video = document.getElementById(' bt_mostrar_video');
+let div_control = document.getElementById('div_control_videos');
 let genero = 0; //0 = hombre, 1 = mujer
 let id_video = '';
-let mp4_video = '';
 
-const apiKeyDId = 'Y29raWtpNzA0MUBidXN0YXllcy5jb20:aC2rsfK9WxHjgWKhvJK2c';
+
+
+
+const apiKeyDId = 'bnVzdGlqaWtub0BndWZ1bS5jb20:adFca_o7gfTMrl79dQgOa';
 const imagen_url = 'https://imgfz.com/i/QgIHbXC.jpeg';
 
+let voz = '';
+let url_imagen = '';
+if(genero == 0){
+    voz = 'es-MX-GerardoNeural';
+    url_imagen = imagen_url;
+}else{
+    voz = 'es-MX-DaliaNeural';
+    url_imagen = imagen_url;
+}
 
-//realizando una peticion a la ia 
-butom_ia.addEventListener('click',async function () {
-    console.log('hola');
-    // console.log(genero)
-    let voz = '';
-    let url_imagen = '';
-    if(genero == 0){
-        voz = 'es-MX-GerardoNeural';
-        url_imagen = imagen_url;
-    }else{
-        voz = 'es-MX-DaliaNeural';
-        url_imagen = imagen_url;
-    }
-    
-   
+console.log('hola desde ia_mover_cara.js');
+
+
+async function postCargarFoto() {
+
+  try {
+
     const formulario = {
-        script: {
-          type: 'text',
-          input: ip_mensaje.value,
-          provider: {
-            type: 'microsoft',
-            voice_id: voz,
-            voice_config: {
-              style: 'Cheerful',
-            },
+      script: {
+        type: 'text',
+        input: ip_mensaje.value,
+        provider: {
+          type: 'microsoft',
+          voice_id: voz,
+          voice_config: {
+            style: 'Cheerful',
           },
         },
-        config: {
-          stitch: true,
-          "driver_expressions": {
-            "expressions": [
-              {
-                "start_frame": 0,
-                "expression": "surprise",
-                "intensity": 1
-              }
-            ]
-          },
+      },
+      config: {
+        stitch: true,
+        "driver_expressions": {
+          "expressions": [
+            {
+              "start_frame": 0,
+              "expression": "surprise",
+              "intensity": 1
+            }
+          ]
         },
-        source_url: url_imagen,
-      };
+      },
+      source_url: url_imagen,
+    };
 
-      //poder animacion de cargando 
-      bt_mostrar_video.classList.remove('hidden');
-      bt_mostrar_video.value = 'Cargando...';
-      
-      await fetch('https://api.d-id.com/talks', {
+    //poder animacion de cargando 
+    bt_mostrar_video.classList.remove('hidden');
+    bt_mostrar_video.classList.remove('btn-green');
+    bt_mostrar_video.classList.add('btn-red');
+
+    let new_video = document.createElement('div');
+    new_video.className = 'border-2 rounded-lg bg-blue-600 h-28 w-28 text-center flex justify-center items-center font-bold';
+    new_video.textContent = 'Subiendo Imagen...';
+
+    let new_img = document.createElement('img');
+
+    new_video.appendChild(new_img);
+    div_control.appendChild(new_video);
+   
+
+    bt_mostrar_video.textContent = 'Subiendo Imagen...';
+
+    await fetch('https://api.d-id.com/talks', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json', // Indicar que estamos enviando datos en formato JSON
@@ -67,14 +84,29 @@ butom_ia.addEventListener('click',async function () {
         .then((data) => data.json())
         .then(async(response) => {
             
-            console.log('entee a response')
-            console.log(response);
-            
-            //vovler a hacer una peticion para obtener el video
+
+          bt_mostrar_video.textContent = 'Procesando video...';
+          //poner el boton en la lista de control de videos 
+          new_video.textContent = 'Procesando video...';
+          new_video.classList.remove('bg-blue-600');
+          // new_img.src = response.source_url;
+          new_img.src = "https://i.pinimg.com/550x/b6/a3/d0/b6a3d082786c2b16f28d24ddf7cb88f2.jpg"
+
+          
             id_video = response.id;
             console.log('id_video:',id_video);
-            bt_mostrar_video.value = 'Procesando...';
-            await fetch('https://api.d-id.com/talks/'+id_video,{
+            // bt_mostrar_video.textContent = 'Procesando...';
+            
+
+            let estado = 'procesando';
+
+            while (estado !== 'done') {
+               // Esperar un tiempo antes de realizar la siguiente verificación
+               console.log();('esperando 5 segundos')
+               await new Promise(resolve => setTimeout(resolve, 5000)); // Esperar 5 segundos
+      
+
+              await fetch('https://api.d-id.com/talks/'+id_video,{ //'tlk_Xye4C03vIGPPx3Bzeo-_5
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json', // Indicar que estamos enviando datos en formato JSON
@@ -84,31 +116,62 @@ butom_ia.addEventListener('click',async function () {
             .then((data) => data.json())
             .then((respuesta) => {
             //quitar animacion y poner video
-            // console.log('respuesta:',respuesta);
-            bt_mostrar_video.value = 'Mostrar video!!';
-            bt_mostrar_video.classList.remove('disabled');
-      
+            console.log('respuesta:',respuesta.status);
+            estado = respuesta.status;
+
+            if(respuesta.status == 'done'){
+                // mp4_video = respuesta.result_url;
+                // console.log('mp4_video:',mp4_video);
+                // ia_video.src = respuesta.result_url;
+              //agregar al array 
+                videos.push(respuesta.result_url);
+                
+                // ia_video.classList.remove('hidden');
+                // document.getElementById('xdxd').classList.add('hidden')
+                bt_mostrar_video.textContent = 'Reproduciendo...';
+                bt_mostrar_video.classList.remove('btn-red');
+                bt_mostrar_video.classList.add('btn-green');
+
+                new_video.textContent = '';
+                new_video.classList.remove('bg-blue-600');
+                new_video.classList.add('bg-red-500');
+                // new_img.src = respuesta.result_url;
+
+
+            }else{
+                console.warn('EL VIDEO SIGUE EN PROCESO')
+            }
+
             })
-            .catch(err => {
-                console.log('entre al error en el 1er post:',err)
-            });
+             
+          }
+          console.log('termine de cargar')
 
         })
         .catch(err => {
             console.log('entre al error:',err)
         });
         console.log('fin xd xd')
+
+
+
+
+    } catch (error) {
+      console.error('Error al realizar el POST:', error);
+  }
+
+}
+
+
+
+//realizando una peticion a la ia 
+butom_ia.addEventListener('click',async function () {
+
+  //ejecuat la funcion post de la ap
+  postCargarFoto()
         
 });
 
-//array de videos
-let videos = [
-      "{{ asset('/videos/video_prueba.mp4')  }}",
-      "{{ asset('/videos/video1.mp4')  }}",
-      "{{ asset('/videos/video2.mp4')  }}",
-      "{{ asset('/videos/video3.mp4')  }}",
-];
-let contador = 0;
 
 let img_hombre = document.getElementById('img_hombre')
 let img_mujer = document.getElementById('img_mujer')
@@ -126,45 +189,6 @@ document.getElementById('bt_mujer').addEventListener('click', function () {
     genero = 1;
     img_hombre.classList.remove('border-blue-600');
     img_mujer.classList.add('border-blue-600');
-});
-
-document.getElementById('bt_pause').addEventListener('click', function () {
-    console.log('click en pause')
-    ia_video.pause();
-});
-
-document.getElementById('bt_play').addEventListener('click', function () {
-    console.log('click en PLAY')
-
-    //elegir video que se reproducira
-    if( contador >= videos.length ){
-       contador = 0;
-    }
-
-    console.log(videos[contador]);
-
-    let video_src = document.getElementById('video_src');
-    video_src.scr = videos[contador];
-
-    ia_video.play();
-    
-});
-
-ia_video.addEventListener('play', function () {
-  console.log('se incio el video');
-  // if( contador = videos.length ){
-  //   contador = 0;
-  //   }
-
-  //   ia_video.src = videos[contador];
-  //   ia_video.play();
-});
-
-
-ia_video.addEventListener('ended', function () {
-    console.log('se termino de reproducir el video');
-    // contador++;
-    // ia_video.play();
 });
 
 
@@ -195,13 +219,3 @@ bt_mostrar_video.addEventListener('click',async function () {
         console.log('termine de cargar')
 });
 
-
-let ai_video2 = document.getElementById('ai_video2');
-ia_video2.addEventListener('play', function () {
-  console.log('se incio el video');
-
-});
-ia_video2.addEventListener('ended', function () {
-    console.log('se termino de reproducir el video');
-
-});
--- resources/views/VistasStream/control.blade.php
+++ resources/views/VistasStream/control.blade.php
@@ -37,33 +37,35 @@
 
                 <div class=" ">
                     <p class="mb-1 font-semibold">Mensaje a reproducir</p>
-                   <div class="flex">
-                    <input id="ip_mensaje" type="text" value="" name="texto" placeholder="escribir"
-                    class="rounded-lg w-[80%]">
-                <button id="butom_ia" type="button"
-                    class="rounded-lg p-2 ml-3 bg-black text-white font-semibold uppercase">
-                    enviar
-                </button>
-                   </div>
+                    <div class="flex">
+                        <input id="ip_mensaje" type="text" value="" name="texto" placeholder="escribir"
+                            class="rounded-lg w-[80%]">
+                        <button id="butom_ia" type="button"
+                            class="rounded-lg p-2 ml-3 bg-black text-white font-semibold uppercase">
+                            enviar
+                        </button>
+                    </div>
                 </div>
 
-                
+
             </div>
             <div class=" w-full flex flex-col justify-between pl-6  ">
                 <div class="bg-gray-800 text-white p-5">
                     <p>Centro de contrl </p>
-                    <div class="flex space-x-5 overflow-x-auto ">
-                        <button id="video_1">
-                            <img class="h-28 w-28" src="{{ asset('/images/video.png') }}" alt="">
-                        </button>
+                    <div id="div_control_videos" class="flex space-x-5 overflow-x-auto ">
 
-                        <button id="video_2">
-                            <img class="h-28 w-28" src="{{ asset('/images/video.png') }}" alt="">
-                        </button>
+                        <div class="" id="video_1">
+                            <img class="h-28 w-28 border-2 rounded-lg border-red-600" src="{{ asset('/images/video.png') }}" alt="">
+                        </div>
 
-                        <button id="video_3">
-                            <img class="h-28 w-28" src="{{ asset('/images/video.png') }}" alt="">
-                        </button>
+                        <div class="border-2 rounded-lg bg-blue-600 h-28 w-28 text-center flex justify-center items-center font-bold " id="video_2">
+                            <img class=" " src="https://i.pinimg.com/550x/b6/a3/d0/b6a3d082786c2b16f28d24ddf7cb88f2.jpg" alt="">
+                            Subiendo imagen...
+                        </div>
+
+                        <div class="" id="video_3">
+                            <img class="h-28 w-28 border-2 rounded-lg " src="{{ asset('/images/video.png') }}" alt="">
+                        </div>
 
                         {{-- <button>
                             <img class="h-28 w-28" src="{{ asset('/images/video.png') }}" alt="">
@@ -76,45 +78,38 @@
                         <button>
                             <img class="h-28 w-28" src="{{ asset('/images/video.png') }}" alt="">
                         </button> --}}
-          
-                    
+
+
                     </div>
 
-                    
+
                 </div>
 
                 <div class="mb-7 flex justify-center">
                     <p class="text-black font-bold text-lg  text-center">
-                        IA Video
+                        {{-- IA Video --}}
                     </p>
-                    <button id=" bt_mostrar_video" class=" btn-green disabled">
-
+                    <button id=" bt_mostrar_video" class="hidden btn-red disabled ">
+                        hoola
                     </button>
                 </div>
 
-                <video id="ia_video2" controls class="hidden bg-black rounded-md border h-80 object-cover">
-                    <!--object-cover -->
-                    <source src="{{ asset('/videos/video_prueba.mp4') }}" type="video/mp4">
+              
+
+
+                <video id="ia_video" controls autoplay class="bg-transparent rounded-md border h-80 object-cover">
+                    {{-- <source id="video_src" src=""  type="video/mp4">  --}}
                     Tu navegador no soporta videos
                 </video>
 
-               
-
-
-
-                 
-                <video id="ia_video" controls autoplay loop class=" rounded-md border h-80 object-cover">
-                    <source id="video_src" src=""  type="video/mp4">
-                    Tu navegador no soporta videos
-                </video>
-                
-
+                <video id="videoPreCargar" style="display: none;"></video>
 
 
                 <div id="xdxd" class="hidden h-64 flex bg-gray-100 justify-center items-center rounded-lg ">
                     <p> no hay video que reproducir</p>
 
                 </div>
+
 
                 <div class="flex justify-around mt-4">
                     <button id="bt_play">
@@ -129,7 +124,103 @@
             </div>
         </div>
 
+        <script>
+            
+
+            console.log('hola desde control');
+            let ia_video = document.getElementById('ia_video');
+            let videoPreCargar = document.getElementById('videoPreCargar');
+            let videos = [
+                "https://fotografia-soft1.s3.amazonaws.com/videos/video1.mp4",
+                "https://fotografia-soft1.s3.amazonaws.com/videos/video2.mp4",
+                "https://fotografia-soft1.s3.amazonaws.com/videos/video3.mp4",
+                
+            ];
+            let contador = 0;
+
+            document.getElementById('bt_play').addEventListener('click', function() {
+                console.log('click en PLAY')
+
+
+                // elegir video que se reproducira
+                if (contador >= videos.length) {
+                    contador = 0;
+                }
+
+                // precargar video
+                // console.log('aux = ' + calciularSigContador());
+                videoPreCargar.src = videos[calciularSigContador()];
+                videoPreCargar.load();
+
+                // console.log('contador = ' + contador);
+                // console.log('contador++ = '+ calciularSigContador());
+                // console.log(videoPreCargar);
+                console.log(videos[contador]);
+                ia_video.src = videos[contador];
+
+                ia_video.load();
+                ia_video.play();
+
+            });
+
+            ia_video.addEventListener('play', function() {
+                console.log('se incio el video');
+                // if( contador = videos.length ){
+                //   contador = 0;
+                //   }
+
+                //   ia_video.src = videos[contador];
+                //   ia_video.play();
+            });
+
+            document.getElementById('bt_pause').addEventListener('click', function() {
+                console.log('click en pause')
+                ia_video.pause();
+            });
+
+
+            ia_video.addEventListener('ended', function() {
+                console.log('se termino de reproducir el video');
+                contador++;
+                // console.log(contador);
+
+                //elegir video que se reproducira
+                if (contador >= videos.length) {
+                    contador = 0;
+                }
+
+                // precargar video
+                console.log('contador = ' + contador);
+                // console.log('contador++ = '+ calciularSigContador());
+                // console.log('contador++ = ' calciularSigContador);
+                videoPreCargar.src = videos[calciularSigContador()];
+                videoPreCargar.load();
+                
+
+
+                console.log(videos[contador]);
+                ia_video.src = videos[contador];
+
     
+                ia_video.load();
+                ia_video.play();
+            });
+
+
+            //function
+            function calciularSigContador() {
+                // console.log('contadorxd: '+contador++);
+                let aux =  contador + 1;
+                console.log(('aux: '+ aux));
+                if (aux >= videos.length) {
+                    aux = 0;
+                }
+            return aux;
+            }
+            
+        </script>
+
+
 
     </div>
 </x-navbar-inquilinos>
