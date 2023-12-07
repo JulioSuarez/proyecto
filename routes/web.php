55019c318cf94079a18d2e625c3c66506a5aa2ee
Cristian Cuellar <ccuellar260@gmail.com>
2023-12-07 16:01:29 -0400

    xd

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Business\AvatarController;
 use App\Http\Controllers\Business\MetodoPagoController;
 use App\Http\Controllers\Business\ProgramController;
 use App\Http\Controllers\ProfileController;
+use App\Models\Suscripcion;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -52,7 +53,17 @@ Route::middleware('auth')->group(function () {
     Route::get('/store',[MetodoPagoController::class, 'store_index'])->name('store.index');
     // Route::get('/store/realizar-pago',[MetodoPagoController::class, 'store_pago'])->name('store.pago');
     
+    Route::get('/hola',function(){
+        Suscripcion::create([
+            'nombre' => 'Plan Basico',
+            'precio' => 1,
+            'stripe_id' => 'price_1OJTfHJrLfX1VoDJWE4uOVib',
+            'foto' => 'https://scontent.fsrz1-2.fna.fbcdn.net/v/t39.30808-6/306696242_2022526374615946_5961266096114267716_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=c83dfd&_nc_ohc=8-mbKNO71pcAX8DFHZg&_nc_ht=scontent.fsrz1-2.fna&oh=00_AfBwDsCf03J0TZbTFD27CTsGhQjDH1K8IA-lHykmsCXqAw&oe=6572BB51',
+        ]);
 
+        return 'hola';
+    });
+   
 
     //julico
     Route::get('/dashboard/admin',[AdministradorController::class, 'dashboard'])->name('admin.dashboard');
